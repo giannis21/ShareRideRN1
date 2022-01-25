62150adc7e65ecdf1e8c4f32f00a9dbd5cd2903f
Giannis Fragoulis <giannisfragoulis21@gmail.com>
2022-01-25 22:09:04 +0200

    σ

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,19 +1,16 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { PostReducer } from './reducers/PostReducer';
 import { AuthReducer } from './reducers/AuthReducer';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers(
-
     { postReducer: PostReducer },
     { authReducer: AuthReducer }
 );
 //const configureStore = createStore(rootReducer)
+const configureStore = () => {
+    return createStore(rootReducer);
+}
 
 
-const configureStore = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
 export default configureStore;
